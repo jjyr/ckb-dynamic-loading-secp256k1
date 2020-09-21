7f91b87902ebb9469887fa6b0eed827e6665a343
jjy <jjyruby@gmail.com>
2020-09-21 12:29:35 +0800

    Support recover pubkey from signature

--- ckb-lib-secp256k1/src/lib.rs
+++ ckb-lib-secp256k1/src/lib.rs
@@ -1,5 +1,7 @@
 #![no_std]
 
+extern crate alloc;
+
 mod code_hashes;
 mod libsecp256k1;
 
--- tests/src/tests.rs
+++ tests/src/tests.rs
@@ -12,6 +12,7 @@ use ckb_tool::ckb_types::{
     prelude::*,
     H256,
 };
+use rand::{thread_rng, Rng};
 use std::fs;
 
 const MAX_CYCLES: u64 = 10_000_000;
@@ -221,6 +222,191 @@ fn test_sign_with_wrong_key() {
 
     // sign
     let tx = sign_tx(tx, &wrong_privkey);
+
+    // run
+    let err = context
+        .verify_tx(&tx, MAX_CYCLES)
+        .expect_err("pass verification");
+    assert_error_eq!(err, ScriptError::ValidationFailure(6),);
+}
+
+#[test]
+fn test_recover_pubkey() {
+    // generate key pair
+    let privkey = Generator::random_privkey();
+    let pubkey = privkey.pubkey().expect("pubkey");
+    let pubkey_hash = blake160(&pubkey.serialize());
+
+    // deploy contract
+    let mut context = Context::default();
+    let contract_bin: Bytes = Loader::default().load_binary("ckb-dynamic-loading-secp256k1");
+    let out_point = context.deploy_cell(contract_bin);
+
+    let secp256k1_bin: Bytes =
+        fs::read("../ckb-miscellaneous-scripts/build/secp256k1_blake2b_sighash_all_dual")
+            .expect("load secp256k1")
+            .into();
+    let secp256k1_out_point = context.deploy_cell(secp256k1_bin);
+    let secp256k1_dep = CellDep::new_builder()
+        .out_point(secp256k1_out_point)
+        .build();
+
+    let secp256k1_data_bin = BUNDLED_CELL.get("specs/cells/secp256k1_data").unwrap();
+    let secp256k1_data_out_point = context.deploy_cell(secp256k1_data_bin.to_vec().into());
+    let secp256k1_data_dep = CellDep::new_builder()
+        .out_point(secp256k1_data_out_point)
+        .build();
+
+    // prepare scripts
+    let lock_script = context
+        .build_script(&out_point, pubkey_hash.to_vec().into())
+        .expect("script");
+    let lock_script_dep = CellDep::new_builder().out_point(out_point).build();
+
+    // prepare cells
+    let input_out_point = context.create_cell(
+        CellOutput::new_builder()
+            .capacity(1000u64.pack())
+            .lock(lock_script.clone())
+            .build(),
+        Bytes::new(),
+    );
+    let input = CellInput::new_builder()
+        .previous_output(input_out_point)
+        .build();
+    let outputs = vec![
+        CellOutput::new_builder()
+            .capacity(500u64.pack())
+            .lock(lock_script.clone())
+            .build(),
+        CellOutput::new_builder()
+            .capacity(500u64.pack())
+            .lock(lock_script)
+            .build(),
+    ];
+
+    let outputs_data = vec![Bytes::new(); 2];
+
+    let mut rng = thread_rng();
+    let mut message = [0u8; 32];
+    rng.fill(&mut message);
+    let sig = privkey.sign_recoverable(&message.into()).expect("sign");
+    let witness = {
+        let mut args = Vec::new();
+        args.extend_from_slice(&message);
+        args.extend_from_slice(&sig.serialize());
+        WitnessArgs::new_builder()
+            .input_type(Some(Bytes::from(args)).pack())
+            .build()
+            .as_bytes()
+            .pack()
+    };
+
+    // build transaction
+    let tx = TransactionBuilder::default()
+        .input(input)
+        .outputs(outputs)
+        .outputs_data(outputs_data.pack())
+        .cell_dep(lock_script_dep)
+        .cell_dep(secp256k1_dep)
+        .cell_dep(secp256k1_data_dep)
+        .witness(witness)
+        .build();
+    let tx = context.complete_tx(tx);
+
+    // run
+    let cycles = context
+        .verify_tx(&tx, MAX_CYCLES)
+        .expect("pass verification");
+    println!("consume cycles: {}", cycles);
+}
+
+#[test]
+fn test_recover_pubkey_with_wrong_signature() {
+    // generate key pair
+    let privkey = Generator::random_privkey();
+    let wrong_privkey = Generator::random_privkey();
+    let pubkey = privkey.pubkey().expect("pubkey");
+    let pubkey_hash = blake160(&pubkey.serialize());
+
+    // deploy contract
+    let mut context = Context::default();
+    let contract_bin: Bytes = Loader::default().load_binary("ckb-dynamic-loading-secp256k1");
+    let out_point = context.deploy_cell(contract_bin);
+
+    let secp256k1_bin: Bytes =
+        fs::read("../ckb-miscellaneous-scripts/build/secp256k1_blake2b_sighash_all_dual")
+            .expect("load secp256k1")
+            .into();
+    let secp256k1_out_point = context.deploy_cell(secp256k1_bin);
+    let secp256k1_dep = CellDep::new_builder()
+        .out_point(secp256k1_out_point)
+        .build();
+
+    let secp256k1_data_bin = BUNDLED_CELL.get("specs/cells/secp256k1_data").unwrap();
+    let secp256k1_data_out_point = context.deploy_cell(secp256k1_data_bin.to_vec().into());
+    let secp256k1_data_dep = CellDep::new_builder()
+        .out_point(secp256k1_data_out_point)
+        .build();
+
+    // prepare scripts
+    let lock_script = context
+        .build_script(&out_point, pubkey_hash.to_vec().into())
+        .expect("script");
+    let lock_script_dep = CellDep::new_builder().out_point(out_point).build();
+
+    // prepare cells
+    let input_out_point = context.create_cell(
+        CellOutput::new_builder()
+            .capacity(1000u64.pack())
+            .lock(lock_script.clone())
+            .build(),
+        Bytes::new(),
+    );
+    let input = CellInput::new_builder()
+        .previous_output(input_out_point)
+        .build();
+    let outputs = vec![
+        CellOutput::new_builder()
+            .capacity(500u64.pack())
+            .lock(lock_script.clone())
+            .build(),
+        CellOutput::new_builder()
+            .capacity(500u64.pack())
+            .lock(lock_script)
+            .build(),
+    ];
+
+    let outputs_data = vec![Bytes::new(); 2];
+
+    let mut rng = thread_rng();
+    let mut message = [0u8; 32];
+    rng.fill(&mut message);
+    let sig = wrong_privkey
+        .sign_recoverable(&message.into())
+        .expect("sign");
+    let witness = {
+        let mut args = Vec::new();
+        args.extend_from_slice(&message);
+        args.extend_from_slice(&sig.serialize());
+        WitnessArgs::new_builder()
+            .input_type(Some(Bytes::from(args)).pack())
+            .build()
+            .as_bytes()
+            .pack()
+    };
+
+    // build transaction
+    let tx = TransactionBuilder::default()
+        .input(input)
+        .outputs(outputs)
+        .outputs_data(outputs_data.pack())
+        .cell_dep(lock_script_dep)
+        .cell_dep(secp256k1_dep)
+        .cell_dep(secp256k1_data_dep)
+        .witness(witness)
+        .build();
+    let tx = context.complete_tx(tx);
 
     // run
     let err = context
